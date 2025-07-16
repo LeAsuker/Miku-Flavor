177ba40645a13c89417c64cd058ff3e3926e0a3c
Asuker <austrituber@seznam.cz>
2025-07-16 11:40:59 +0200

    Updated and simplified the installation script

--- miku_flavor_install.sh
+++ miku_flavor_install.sh
@@ -6,20 +6,20 @@ apt -qq install dmenu
 apt -qq install herbstluftwm
 mkdir -p $HOME/.config/herbstluftwm
 cd $HOME/.config/herbstluftwm
-wget -O autostart https://raw.githubusercontent.com/ASTROfocs/configs/refs/heads/main/autostart && echo "HLWM config downloaded!"
+wget -O autostart https://raw.githubusercontent.com/ASTROfocs/Miku-Flavor/refs/heads/main/Configs/autostart
 chmod u+x autostart
 
 apt -qq install polybar
 mkdir -p $HOME/.config/polybar
 cd $HOME/.config/polybar
-wget -O config.ini https://raw.githubusercontent.com/ASTROfocs/configs/refs/heads/main/config.ini && echo "Polybar config downloaded!"
+wget -O https://raw.githubusercontent.com/ASTROfocs/Miku-Flavor/refs/heads/main/Configs/config.ini
 
 apt -qq install picom
 mkdir -p $HOME/.config/picom
 cd $HOME/.config/picom
-wget -O picom.conf https://raw.githubusercontent.com/ASTROfocs/configs/refs/heads/main/picom.conf
+wget -O picom.conf https://raw.githubusercontent.com/ASTROfocs/Miku-Flavor/refs/heads/main/Configs/picom.conf
 
 apt -qq install alacritty
 mkdir -p $HOME/.config/alacritty
 cd $HOME/.config/alacritty
-wget -O alacritty.toml https://raw.githubusercontent.com/ASTROfocs/configs/refs/heads/main/alacritty.toml
+wget -O alacritty.toml https://raw.githubusercontent.com/ASTROfocs/Miku-Flavor/refs/heads/main/Configs/alacritty.toml
